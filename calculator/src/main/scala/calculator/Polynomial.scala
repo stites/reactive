d9dcdaf2986aa87db80c34b0059f24a5cf3c8bf6
stites <sam@stites.io>
2015-04-27 22:29:28 -0700

    fix - polynomial calculations

--- calculator/src/main/scala/calculator/Polynomial.scala
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -3,16 +3,22 @@ package calculator
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    Signal( b() + b() - 4 * a() * c() )
+    Signal {
+      (b() * b()) - (4 * a() * c())
+    }
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
     Signal {
-      Set(
-        (- b() + math.sqrt( delta() ) ) / (2*a()),
-        (- b() - math.sqrt( delta() ) ) / (2*a())
-      )
+      delta() match {
+        case delt if delt > 0 => Set(
+          (- b() + math.sqrt( delta() ) ) / (2*a()),
+          (- b() - math.sqrt( delta() ) ) / (2*a())
+        )
+        case delt if delt == 0 => Set((-b()) / (2 * a()))
+        case _ => Set()
+      }
     }
   }
 }
