bb12dd5d300e3f17f4dafbb9a25df778d7e1b484
stites <sam@stites.io>
2015-05-07 01:00:23 -0700

    write - response

--- nodescala/src/main/scala/nodescala/nodescala.scala
+++ nodescala/src/main/scala/nodescala/nodescala.scala
@@ -29,7 +29,12 @@ trait NodeScala {
    *  @param token        the cancellation token
    *  @param body         the response to write back
    */
-  private def respond(exchange: Exchange, token: CancellationToken, response: Response): Unit = ???
+  private def respond(exchange: Exchange, token: CancellationToken, response: Response): Unit = {
+    while (token.nonCancelled && response.hasNext) {
+      exchange.write(response.next)
+    }
+    exchange.close()
+  }
 
   /** A server:
    *  1) creates and starts an http listener
